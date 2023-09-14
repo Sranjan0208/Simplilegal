4c0dc8dc00a5a341d17d508485735d14319d3f3e
Sagnik <santrasagnik10@gmail.com>
2023-09-15 01:42:07 +0530

    Added changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -132,7 +132,7 @@ function Nav() {
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Lawyers
-                </a>
+                </a> 
 
                 <a
                   href="#"
